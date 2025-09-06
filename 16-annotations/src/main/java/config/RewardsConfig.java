f9c532ec00b44ae241da3ae1565894949421a684
diego <dscarcaba@gmail.com>
2025-09-06 18:37:26 +0200

    primera fase terminada_prueba

--- 16-annotations/src/main/java/config/RewardsConfig.java
+++ 16-annotations/src/main/java/config/RewardsConfig.java
@@ -2,6 +2,7 @@ package config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
@@ -14,13 +15,8 @@ import rewards.internal.reward.RewardRepository;
 
 import javax.sql.DataSource;
 
-/**
- * TODO-07: Perform component-scanning and run the test again
- * - Add an appropriate annotation to this class to cause component scanning.
- * - Set the base package to pick up all the classes we have annotated so far.
- * - Save all changes, Re-run the RewardNetworkTests.  It should now pass.
- */
 @Configuration
+@ComponentScan(basePackages = "rewards.internal")
 public class RewardsConfig {
 
 }
